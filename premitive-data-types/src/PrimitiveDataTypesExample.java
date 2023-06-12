8e762f95250357feda999b3ca2adce1bf5ce641d
pratik powar <pratik9885@gmail.com>
2023-06-12 22:58:47 +0530

    adding data type example file

---
+++ premitive-data-types/src/PrimitiveDataTypesExample.java
@@ -0,0 +1,36 @@
+public class PrimitiveDataTypesExample {
+    public static void main(String[] args) {
+        // boolean
+        boolean isTrue = true;
+        boolean isFalse = false;
+        System.out.println("Boolean: " + isTrue + ", " + isFalse);
+
+        // byte
+        byte myByte = 100;
+        System.out.println("Byte: " + myByte);
+
+        // short
+        short myShort = 1000;
+        System.out.println("Short: " + myShort);
+
+        // int
+        int myInt = 100000;
+        System.out.println("Int: " + myInt);
+
+        // long
+        long myLong = 10000000000L;
+        System.out.println("Long: " + myLong);
+
+        // float
+        float myFloat = 3.14f;
+        System.out.println("Float: " + myFloat);
+
+        // double
+        double myDouble = 3.14159;
+        System.out.println("Double: " + myDouble);
+
+        // char
+        char myChar = 'A';
+        System.out.println("Char: " + myChar);
+    }
+}
