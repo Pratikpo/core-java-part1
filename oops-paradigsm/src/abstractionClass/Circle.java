40473b527454d26e171c8d89174bf3ed22abb6cf
pratik powar <pratik9885@gmail.com>
2023-08-05 13:55:23 +0530

    AbstractionClass

---
+++ oops-paradigsm/src/abstractionClass/Circle.java
@@ -0,0 +1,20 @@
+package abstractionClass;
+
+public class Circle extends Shape{
+    float radius;
+
+    public Circle(float radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public void draw() {
+
+        System.out.println("I'm drawing Circle");
+    }
+
+    @Override
+    public double calculateArea() {
+        return 3.14*radius*radius;
+    }
+}
---
+++ oops-paradigsm/src/abstractionClass/Rectangle.java
@@ -0,0 +1,21 @@
+package abstractionClass;
+
+public class Rectangle extends Shape{
+    float length;
+    float breadth;
+
+    public Rectangle(float length, float breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("I'm drawing Rectangle");
+    }
+
+    @Override
+    public double calculateArea() {
+        return length*breadth;
+    }
+}
