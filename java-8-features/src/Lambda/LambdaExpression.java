48ab158d1ab058ba29d78037da3d7aa665f5d629
Pratikpo <pratik9885@gmail.com>
2023-10-30 23:48:58 +0530

    java 8 features

--- java-8-features/src/Lambda/LambdaExpression.java
+++ java-8-features/src/Lambda/LambdaExpression.java
@@ -3,17 +3,21 @@ package Lambda;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class LambdaExpression {
     public static void main(String[] args) {
         List<String> grocery=new ArrayList<>();
         grocery.add("sugar");
         grocery.add("salt");
         grocery.add("soup");
+        grocery.add("sugar");
         for (String name:grocery){
             System.out.println(name);
         }
 
-        grocery.forEach(System.out::println);
-        grocery.forEach(System.out::println);//java 8 feature lambda expression
+       // grocery.forEach(System.out::println);
+       // grocery.forEach(System.out::println);//java 8 feature lambda expression
+        grocery.forEach(item-> System.out.println(item));
+
     }
 }
---
+++ java-8-features/src/Streamexample/StreamExample.java
@@ -0,0 +1,27 @@
+package Streamexample;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Stream;
+
+public class StreamExample {
+    public static void main(String[] args) {
+        List<String> guestList=new ArrayList<>();
+        guestList.add("Vikram");
+        guestList.add("Karan");
+        guestList.add("Pratik");
+        //streams are used to replace for while loop.
+        Stream<String> stringStream=guestList.stream();
+        stringStream
+                .filter(s->s.length()>=5)
+                .map(s->{
+                    System.out.println("s is : "+s);
+                    return s.toLowerCase();
+                })
+                .forEach(item-> System.out.println("item in list "+item));
+
+        //the execution of elements occurs in parallel
+        //that means if streams contains 5 elements then for each entire stream pipeline get executed
+    }
+}
